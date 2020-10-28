ca611d1a6b9299bdf9ae4c01ebe32f994766e453
isatyakeerthi <satya10.sashtry@gmail.com>
2020-10-28 20:12:16 +0530

    Adding Final Practice code

---
+++ 28Oct/src/com/finalpractice/PracticeFinal.java
@@ -0,0 +1,16 @@
+package com.finalpractice;
+
+public final class PracticeFinal {
+
+	
+		int a = 10;
+		int b = 20;
+		
+		public void m1() {
+			System.out.println("Hi! I'm in M1");
+			
+		}
+
+	}
+
+
---
+++ 28Oct/src/com/finalpractice/PracticeFinal2.java
@@ -0,0 +1,5 @@
+package com.finalpractice;
+
+public class PracticeFinal2 {
+
+}
